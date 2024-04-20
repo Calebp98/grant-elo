793acdc557188fb1baec6ebb92f5b99ec6117d84
Caleb Parikh <parikhcaleb@gmail.com>
2024-04-20 12:38:43 -0700

    switched to real database

--- grant_elo.py
+++ grant_elo.py
@@ -1,4 +1,3 @@
-
 # streamlit_app.py
 import streamlit as st
 import pandas as pd
@@ -9,13 +8,16 @@ import seaborn as sns
 # Initialize connection.
 conn = st.connection("supabase", type=SupabaseConnection)
 
+
 def get_data():
-    result = conn.query("*", table="test", ttl="0").execute()
+    result = conn.query("*", table="grants_elo", ttl="0").execute()
     df = pd.DataFrame(result.data)
     return df
 
+
 def update_data(df):
-    conn.table("test").upsert(df.to_dict(orient="records")).execute()
+    conn.table("grants_elo").upsert(df.to_dict(orient="records")).execute()
+
 
 st.title("Elo Everygrant")
 
@@ -24,7 +26,7 @@ if "df" not in st.session_state:
 
 df = st.session_state.df
 
-show_elo = st.checkbox("Show Current ELO", value=False)
+show_elo = st.checkbox("Show Current Elo", value=False)
 
 # Add checkboxes for each fund
 funds = df["fund"].unique()
@@ -44,6 +46,7 @@ filtered_df = df[df["fund"].isin(selected_funds)]
 
 if "random_rows" not in st.session_state:
     st.session_state.random_rows = filtered_df.sample(n=2)
+
 
 def update_elo(winner):
     grant_a = st.session_state.random_rows.iloc[0]
@@ -69,8 +72,10 @@ def update_elo(winner):
     st.session_state.df = filtered_df
     update_data(filtered_df)
 
+
 def skip():
     st.session_state.random_rows = filtered_df.sample(n=2)
+
 
 left_half, right_half = st.columns(2)
 col1, col2, col3 = st.columns(3)
@@ -83,9 +88,8 @@ with left_half:
     a.write(grant_a.fund)
     a.write(grant_a.description)
     a.write(f"${grant_a.amount:,}")
-    if show_elo: 
+    if show_elo:
         a.write(f"ELO: {grant_a['elo']:.0f}")
-
 
 
 with right_half:
@@ -94,18 +98,20 @@ with right_half:
     b.write(grant_b.fund)
     b.write(grant_b.description)
     b.write(f"${grant_b.amount:,}")
-    if show_elo: 
+    if show_elo:
         b.write(f"ELO: {grant_b['elo']:.0f}")
 
 with col1:
-    st.button("A", on_click=update_elo, args=("A",),use_container_width=True)
+    st.button("A", on_click=update_elo, args=("A",), use_container_width=True)
 
 with col2:
-    st.button("Draw", on_click=update_elo, args=("Draw",),use_container_width=True)
-    st.button("Skip (No ELO Change)", on_click=skip,use_container_width=True, type="primary")
+    st.button("Draw", on_click=update_elo, args=("Draw",), use_container_width=True)
+    st.button(
+        "Skip (No ELO Change)", on_click=skip, use_container_width=True, type="primary"
+    )
 
 with col3:
-    st.button(f"B", on_click=update_elo, args=("B",),use_container_width=True)
+    st.button(f"B", on_click=update_elo, args=("B",), use_container_width=True)
 
 with st.expander("Leaderboard"):
     st.table(
@@ -115,6 +121,7 @@ with st.expander("Leaderboard"):
         .rename(columns={"elo": "ELO", "fund": "Fund", "description": "Description"})
     )
 
+
 def plot_elo_distribution():
     fig, ax = plt.subplots(figsize=(8, 4))
     sns.kdeplot(data=filtered_df, x="elo", fill=True, ax=ax)
@@ -123,7 +130,9 @@ def plot_elo_distribution():
     ax.set_title("ELO Distribution")
     st.pyplot(fig)
 
+
 plot_elo_distribution()
+
 
 def plot_rating_frequency_distribution():
     fig, ax = plt.subplots(figsize=(8, 4))
@@ -132,6 +141,7 @@ def plot_rating_frequency_distribution():
     ax.set_ylabel("Frequency")
     ax.set_title("Rating Frequency Distribution")
     st.pyplot(fig)
+
 
 plot_rating_frequency_distribution()
 
